e920de365667a5e3ad6c4433e7056330adde33be
Ollie-Judge <ollie.judge@hotmail.com>
2022-12-15 16:45:03 +0000

    generated a random number between 0 and 1

---
+++ script.js
@@ -0,0 +1,6 @@
+let headsTails = Math.round(Math.random());
+console.log(headsTails);
+
+// 1 or 0 has been generated and so now need to get it into an array
+//which will then send the img tag a src of either the first or
+//second array item which is the heads or tails photo
